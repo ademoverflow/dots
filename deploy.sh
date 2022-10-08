dbaeafa8b6bb4d853ac0c16845753168f892398e
Adem Usta <mr.usta.adem@gmail.com>
2022-10-08 15:03:43 +0200

    deploy picom

--- deploy.sh
+++ deploy.sh
@@ -17,3 +17,7 @@ ln -s $PWD/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 # i3
 rm -f ~/.i3/config
 ln -s $PWD/.config/i3/config ~/.i3/config
+
+# picom
+rm -f ~/.config/picom.conf
+ln -s $PWD/.config/picom/picom.conf ~/.config/picom.conf
