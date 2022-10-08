c56fd670828772c3ad6b67ba20c833c489b0a70d
Adem Usta <mr.usta.adem@gmail.com>
2022-10-08 14:28:26 +0200

    deploy dot files script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# ===================================
+#        Deploy config files
+# ===================================
+
+set -euxo pipefail
+
+# Gitconfig
+rm -f ~/.gitconfig
+ln -s $PWD/.config/git/.gitconfig ~/.gitconfig
+
+# Alacritty
+rm -f ~/.config/alacritty/alacritty.yml
+ln -s $PWD/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+# i3
+rm -f ~/.i3/config
+ln -s $PWD/.config/i3/config ~/.i3/config
