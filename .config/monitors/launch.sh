2d52b629059f0d72a02fadb8ae1139e8167b898f
Adem Usta <adem_ext@anotherbrain.ai>
2022-11-25 13:55:05 +0100

    ok

--- .config/monitors/launch.sh
+++ .config/monitors/launch.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-
-xrandr --output eDP-1 --off --output DVI-I-5-4 --off --output DVI-I-4-3 --off --output DVI-I-3-2 --off --output DVI-I-2-1 --primary --mode 1920x1080 --pos 1930x0 --rotate normal --output HDMI-1-0 --mode 1920x1080 --pos 0x0 --rotate normal
+#!/bin/sh
+xrandr --output eDP-1 --off --output DVI-I-5-4 --off --output DVI-I-4-3 --off --output DVI-I-3-2 --off --output DVI-I-2-1 --off --output HDMI-1-0 --primary --mode 3840x1080 --pos 0x0 --rotate normal
