47be64d02a75e52f70ec4c78cb3605dc07ceacb7
Adem Usta <mr.usta.adem@gmail.com>
2022-11-27 21:25:00 +0100

    good

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,7 @@ if type "xrandr"; then
   # Primary monitor first
   PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
   MONITOR=$PRIMARY polybar --reload top &
+  sleep 0.5
 
   # Others
   OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
