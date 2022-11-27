ef68aa5e21860beb57376956897f6c127e55233b
Adem Usta <mr.usta.adem@gmail.com>
2022-11-27 17:28:43 +0100

    launch polybar on all monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,5 +3,10 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar -r top
+if type "xrandr"; then
+  for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$monitor polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
